18223217a85e9af9a7046341f6d5e744f8f500c9
alejandroileal <aleal1202@outlook.com>
2023-11-07 15:32:41 +0100

    Remove commented code

--- src/components/card.list/card.list.tsx
+++ src/components/card.list/card.list.tsx
@@ -1,8 +1,3 @@
-// import { ApiRepo } from '../../services/api.repo';
-
 export function CardList() {
-  // const repo = new ApiRepo();
-  // const classes = repo.getClasses();
-  // const bosses = repo.getBosses()
   return <ul></ul>;
 }
