e8a91344b29e34881746073f9b74e2198f853757
Carlos Solis <mrsoliscalderon@gmail.com>
2023-11-07 11:18:12 +0100

    Add updates for css

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,13 +3,17 @@ import { Header } from '../header/header';
 import { Characters } from '../characters/characters';
 import { CardList } from '../card.list/card.list';
 import { Footer } from '../footer/footer';
+import { FilterButton } from '';
 
 export default function App() {
   return (
     <>
       <Header></Header>
       <Characters>
-        <CardList></CardList>
+        <>
+          <FilterButton></FilterButton>
+          <CardList></CardList>
+        </>
       </Characters>
       <Footer></Footer>
     </>
---
+++ src/components/detail.buttton/detail.button.tsx
@@ -0,0 +1,10 @@
+export function DetailButton() {
+  return (
+    <div className="details-button">
+      <label htmlFor="showdetails">showdetails</label>
+      <select name="showdetails" id="showdetails">
+        <option value="showdetails">showdetails</option>
+      </select>
+    </div>
+  );
+}
