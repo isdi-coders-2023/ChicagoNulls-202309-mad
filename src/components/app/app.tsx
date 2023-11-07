54affc8e1137bdc167ca5eea5a9466e8cc300842
alejandroileal <aleal1202@outlook.com>
2023-11-07 14:15:07 +0100

    Update app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,17 +3,12 @@ import { Header } from '../header/header';
 import { Characters } from '../characters/characters';
 import { CardList } from '../card.list/card.list';
 import { Footer } from '../footer/footer';
-import { Card } from '../card/card';
-import { ApiRepo } from '../../services/api.repo';
-
 export default function App() {
-  const repo = new ApiRepo();
   return (
     <>
       <Header></Header>
-      <Card></Card>
       <Characters>
-        <CardList repo={repo}></CardList>
+        <CardList></CardList>
       </Characters>
       <Footer></Footer>
     </>
