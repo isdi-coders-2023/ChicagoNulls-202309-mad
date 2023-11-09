1f78d1b1cdaf8252b3fe3fa42d92ccb4c6113c80
[Ivana Speziale] <ivana.robledo.speziale@gmail.com>
2023-11-09 22:18:13 +0100

    Update CSS in card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,5 +1,5 @@
 import { CharacterStructure } from '../../models/eldenring.api';
-
+import './card.scss';
 type Props = {
   character: CharacterStructure;
 };
@@ -8,8 +8,11 @@ export function Card({ character }: Props) {
   const imageType = '.png';
 
   return (
-    <li className="Card">
-      <img src={imageUrl + character.id + imageType} alt={character.name} />
-    </li>
+    <div className="card-container">
+      <li className="Card">
+        <img src={imageUrl + character.id + imageType} alt={character.name} />
+      </li>
+      <button className="details">Show Details</button>
+    </div>
   );
 }
