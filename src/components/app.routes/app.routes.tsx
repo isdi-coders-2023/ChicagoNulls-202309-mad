1ee6802d2a1e017c4e3b1503a761c2e166ad9074
alejandroileal <aleal1202@outlook.com>
2023-11-10 11:40:53 +0100

    Fix audir error at AppRoutes

--- src/components/app.routes/app.routes.tsx
+++ src/components/app.routes/app.routes.tsx
@@ -12,18 +12,18 @@ const ErrorPage = lazy(() => import('../pages/error.page/error.page'));
 const DetailsPage = lazy(() => import('../pages/details.page/details.page'));
 export function AppRoutes() {
   return (
- <Suspense>
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/home" element={<Home></Home>}></Route>
-          <Route
-            path="/custom-library"
-            element={<CustomLibrary></CustomLibrary>}
-          ></Route>
-          <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
-          <Route path="/details" element={<DetailsPage></DetailsPage>}></Route>
-        </Routes>
-      </Suspense>
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route
+          path="/custom-library"
+          element={<CustomLibrary></CustomLibrary>}
+        ></Route>
+        <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
+        <Route path="/details" element={<DetailsPage></DetailsPage>}></Route>
+      </Routes>
+    </Suspense>
   );
 }
 
