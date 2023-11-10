73d0d5525396a086e4709a7c6ea789ad363f30a1
Sergi <sergi20031005@gmail.com>
2023-11-10 16:49:28 +0100

    Fix testing code duplicate

--- src/components/app.routes/app.routes.test.tsx
+++ src/components/app.routes/app.routes.test.tsx
@@ -1,98 +1,74 @@
 import { render, screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
-import { MemoryRouter as Router } from 'react-router-dom'; // Importa MemoryRouter
+import { MemoryRouter as Router } from 'react-router-dom';
 import { AppRoutes } from './app.routes';
 import '@testing-library/jest-dom';
+import { InitialEntry } from '@remix-run/router';
 
-jest.mock('../../components/pages/custom.library/custom.library', () => {
+const renderComponent = async (route: InitialEntry) => {
+  await act(async () => {
+    render(
+      <Router initialEntries={[route]}>
+        <AppRoutes />
+      </Router>
+    );
+  });
+};
+
+const mockComponent = (content: React.ReactNode) => {
   return {
     __esModule: true,
     default: () => {
-      return (
-        <div data-testid="mocked-custom-library">
-          Mocked Custom Library Component
-        </div>
-      );
+      return content;
     },
   };
+};
+
+jest.mock('../../components/pages/custom.library/custom.library', () => {
+  return mockComponent(
+    <div data-testid="mocked-custom-library">
+      Mocked Custom Library Component
+    </div>
+  );
 });
 
 jest.mock('../pages/character.page/character.page', () => {
-  return {
-    __esModule: true,
-    default: () => {
-      return <p data-testid="mocked-home">Mocked Home Component</p>;
-    },
-  };
+  return mockComponent(<p data-testid="mocked-home">Mocked Home Component</p>);
 });
 
 jest.mock('../pages/error.page/error.page', () => {
-  return {
-    __esModule: true,
-    default: () => {
-      return (
-        <span data-testid="mocked-error-page">Mocked Error Page Component</span>
-      );
-    },
-  };
+  return mockComponent(
+    <span data-testid="mocked-error-page">Mocked Error Page Component</span>
+  );
 });
 
 jest.mock('../pages/details.page/details.page', () => {
-  return {
-    __esModule: true,
-    default: () => {
-      return (
-        <h1 data-testid="mocked-details-page">Mocked Details Page Component</h1>
-      );
-    },
-  };
+  return mockComponent(
+    <h1 data-testid="mocked-details-page">Mocked Details Page Component</h1>
+  );
 });
 
 describe('When we render', () => {
   test('the component should render Custom Library', async () => {
-    await act(async () => {
-      render(
-        <Router initialEntries={['/custom-library']}>
-          <AppRoutes />
-        </Router>
-      );
-    });
+    await renderComponent('/custom-library');
     const element = screen.getByTestId('mocked-custom-library');
     expect(element).toBeInTheDocument();
   });
 
   test('the component should render Home', async () => {
-    await act(async () => {
-      render(
-        <Router initialEntries={['/home']}>
-          <AppRoutes />
-        </Router>
-      );
-    });
+    await renderComponent('/home');
     const element = screen.getByTestId('mocked-home');
     expect(element).toBeInTheDocument();
   });
 
   test('the component should render Error Page', async () => {
-    await act(async () => {
-      render(
-        <Router initialEntries={['/*']}>
-          <AppRoutes />
-        </Router>
-      );
-    });
+    await renderComponent('/*');
     const element = screen.getByTestId('mocked-error-page');
     expect(element).toBeInTheDocument();
   });
 
-  test('the component should render Error Page', async () => {
-    await act(async () => {
-      render(
-        <Router initialEntries={['/details/:id/']}>
-          <AppRoutes />
-        </Router>
-      );
-    });
+  test('the component should render Details Page', async () => {
+    await renderComponent('/details/:id/');
     const element = screen.getByTestId('mocked-details-page');
     expect(element).toBeInTheDocument();
   });
