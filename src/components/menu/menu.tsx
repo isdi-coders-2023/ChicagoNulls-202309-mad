13f08a51c0a836aae001faab7970565ed70cf48c
[Ivana Speziale] <ivana.robledo.speziale@gmail.com>
2023-11-10 16:32:31 +0100

    Fix bug in menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,25 +1,3 @@
-/* import { Link } from 'react-router-dom';
-import { MenuOption } from '../../models/menu.option.type';
-import './menu.scss';
- */
-/* type Props = {
-  options: MenuOption[];
-};
-
-export function Menu({ options }: Props) {
-  return (
-    <nav role="menu">
-      <img src="../bars.svg" alt="icono de menu plegable" />
-      <ul>
-        {options.map((item) => (
-          <li key={item.label}>
-              <Link to={item.path}>{item.label}</Link>   
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-} */
 import { Link } from 'react-router-dom';
 import { MenuOption } from '../../models/menu.option.type';
 import './menu.scss';
