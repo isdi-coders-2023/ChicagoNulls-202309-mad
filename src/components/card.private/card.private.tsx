a315ff317e727c7899e7e42161bbb1a3896fda68
alejandroileal <aleal1202@outlook.com>
2023-11-11 17:40:54 +0100

    Add card private testing

--- src/components/card.private/card.private.tsx
+++ src/components/card.private/card.private.tsx
@@ -1,16 +1,25 @@
-export function CardPrivate() {
+import { CharacterStructure } from '../../models/eldenring.api';
+import './card.private.scss';
+type Props = {
+  character: CharacterStructure;
+};
+export function CardPrivate({ character }: Props) {
   return (
     <div className="card-container">
       <li className="Card">
-        <img />
-        <p>Character name</p>
         <div className="card-buttons-container">
           <button>
-            <img src="" alt="" srcset="" />
+            <img src="delete-button.svg" alt="" />
+          </button>
+          <button>
+            <img src="edit-button.svg" alt="" />
           </button>
         </div>
+        <div>
+          <img src="https://eldenring.fanapis.com/images/classes/17f69874f7bl0i32gmqaffmbfral8f.png" />
+        </div>
+        <button className="details">{character.name}</button>
       </li>
-      <button className="details"></button>
     </div>
   );
 }
