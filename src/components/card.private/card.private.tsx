1fd368387d2d5429c4525e5067d62d8002f0e672
alejandroileal <aleal1202@outlook.com>
2023-11-11 17:12:35 +0100

    Create private button component

---
+++ src/components/card.private/card.private.tsx
@@ -0,0 +1,16 @@
+export function CardPrivate() {
+  return (
+    <div className="card-container">
+      <li className="Card">
+        <img />
+        <p>Character name</p>
+        <div className="card-buttons-container">
+          <button>
+            <img src="" alt="" srcset="" />
+          </button>
+        </div>
+      </li>
+      <button className="details"></button>
+    </div>
+  );
+}
